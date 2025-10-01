785a010a6130abf85be6f2d5713e3ef697bd8395
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-10-01 13:04:41 -0400

    Removed next method from CharacterId;

--- src/character/mod.rs
+++ src/character/mod.rs
@@ -1,10 +1,6 @@
 //! Character data.
 
 
-use core::sync::atomic::{
-    AtomicU32,
-    Ordering as AtomicOrdering
-};
 use bevy_ecs::component::Component;
 
 
@@ -15,26 +11,6 @@ mod pos;
 pub use pos::*;
 
 
-static NEXT_CHARACTER_ID : AtomicU32 = AtomicU32::new(0);
-
-
 /// A character's networked ID used to track and update the character later.
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Component)]
-pub struct CharacterId(u32);
-
-impl CharacterId {
-
-    /// Create a new [`CharacterId`] with the next character ID.
-    #[inline]
-    pub fn next() -> Self { Self(NEXT_CHARACTER_ID.fetch_add(1, AtomicOrdering::Relaxed)) }
-
-    /// Returns the inner value as a primitive type.
-    #[inline]
-    pub const fn as_u32(&self) -> u32 { self.0 }
-
-}
-
-impl Default for CharacterId {
-    #[inline]
-    fn default() -> Self { Self::next() }
-}
+pub struct CharacterId(pub u32);

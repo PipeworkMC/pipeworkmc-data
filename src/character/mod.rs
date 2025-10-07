9e774d1d82401e48210ff3caac43ffa2b27a5dfd
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-10-06 22:34:00 -0400

    Impl PacketDecode for CharacterId;

--- src/character/mod.rs
+++ src/character/mod.rs
@@ -1,6 +1,18 @@
 //! Character data.
 
 
+use pipeworkmc_codec::{
+    decode::{
+        PacketDecode,
+        DecodeIter
+    },
+    varint::{
+        VarInt,
+        VarIntDecodeError
+    }
+};
+
+
 #[cfg(feature = "generated")]
 include!("../../../pipeworkmc-vanilla-datagen/output/generated/entity_type.rs");
 
@@ -11,3 +23,13 @@ pub use pos::*;
 /// A character's networked ID used to track and update the character later.
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct CharacterId(pub u32);
+
+impl PacketDecode for CharacterId {
+    type Error = VarIntDecodeError;
+
+    #[inline]
+    fn decode<I>(iter : &mut DecodeIter<I>) -> Result<Self, Self::Error>
+    where
+        I : ExactSizeIterator<Item = u8>
+    { Ok(Self(<VarInt<u32>>::decode(iter)?.0)) }
+}
